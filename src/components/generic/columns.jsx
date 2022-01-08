092c1c0adfe470ddbadc129678a5f0beb52ddef0
NCHLab <mohamudc1@hotmail.com>
2022-01-08 20:03:16 +0000

    table updated, buttons and disabled effects

--- src/components/generic/columns.jsx
+++ src/components/generic/columns.jsx
@@ -3,32 +3,44 @@ import { parseISO, format } from 'date-fns'
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import DoNotDisturbIcon from '@mui/icons-material/DoNotDisturb';
 
+import { numberFormat } from './utils'
+
 export const COLUMNS = [
     {
         Header: 'ID',
         accessor: 'id',
+        width: 50,
         Cell: (row) => {
             return <div style={{ textAlign: "center" }}>{row.value}</div>
         }
     },
     {
         Header: 'User',
-        accessor: 'user_id'
+        accessor: 'user_id',
+        width: 100,
+        Cell: (row) => {
+            return <div style={{ textAlign: "center" }}>{row.value}</div>
+        }
     },
     {
         Header: 'Paid',
-        accessor: 'paid'
+        accessor: 'paid',
+        Cell: (row) => {
+            return <div style={{ textAlign: "center" }}>{numberFormat(row.value)}</div>
+        }
     },
     {
         Header: 'Reason',
-        accessor: 'reason'
+        accessor: 'reason',
+        width: 300,
     },
     {
         Header: 'Date',
         accessor: 'date',
+        width: 300,
         Cell: (row) => {
             const custom_date = format(parseISO(row.value), "do MMM yyyy HH:mm:ss")
-            return <span>{custom_date}</span>
+            return <div style={{ textAlign: "center" }}>{custom_date}</div>
         }
     },
     {
--- src/components/paymentsComponent/Payments.jsx
+++ src/components/paymentsComponent/Payments.jsx
@@ -19,11 +19,18 @@ import { TableRow } from '@mui/material';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 
 
+import AddIcon from '@mui/icons-material/Add';
+import EditAttributesIcon from '@mui/icons-material/EditAttributes';
+import ToggleOffIcon from '@mui/icons-material/ToggleOff';
+import ToggleOnIcon from '@mui/icons-material/ToggleOn';
+
+
+import EditForm from './EditForm'
 
 const Payments = () => {
 
     const { tableData, COLUMNS } = usePayments();
-    const [disableEditing, setDisableEditing] = useState(true)
+    const [enableEditing, setEnableEditing] = useState(false)
 
     // useEffect(() => {
     //     getInfo()
@@ -69,8 +76,17 @@ const Payments = () => {
 
 
     const handleEditing = () => {
-        setDisableEditing(prevVal => !prevVal)
-        console.log(disableEditing)
+        // setEnableEditing(prevVal => !prevVal)
+
+        setEnableEditing(prevVal => {
+            console.log(!prevVal)
+            return !prevVal
+        })
+
+    }
+
+    const handleAddNew = () => {
+
     }
 
     const {
@@ -83,36 +99,46 @@ const Payments = () => {
 
     const isEven = (idx) => idx % 2 === 0;
 
-    const styles = {
-        tr: {
-            background: "#ffffff",
-            '&:hover': {
-                background: "#000",
-            }
-        }
-    };
-
-
 
     return (
         <Container maxWidth='lg'>
+
+            <EditForm />
+
+
             <Grid container
                 direction="row"
                 alignItems="center"
                 mt='5vh'
             >
-                <Grid item xs={12} pb={0}>
+
+                <Grid item xs={1.4} pb={0}>
                     <Button
                         variant="outlined"
-                        onClick={() => { handleEditing() }}
+                        onClick={handleAddNew}
                         sx={{
-                            my: 2, color: '#dedede', border: '2px solid #000000', backgroundImage: 'linear-gradient(to left, rgba(166, 0, 0, 0.5), rgba(43, 43, 43, 0.5))',
+                            my: 1, color: '#dedede', border: '2px solid #000000', backgroundImage: 'linear-gradient(to left, rgba(166, 0, 0, 0.8), rgba(43, 43, 43, 0.8))',
+                            ':hover': {
+                                backgroundImage: `linear-gradient(to right, #a60000, #2b2b2b)`,
+                                color: '#36c9ff',
+                                border: '2px solid #000000'
+                            }
+                        }}><AddIcon /> Add New</Button>
+                </Grid>
+
+                <Grid item xs={2} pb={0}>
+                    <Button
+                        variant="outlined"
+                        onClick={handleEditing}
+                        sx={{
+                            my: 1, color: '#dedede', border: '2px solid #000000', backgroundImage: enableEditing ? 'linear-gradient(to left, rgba(166, 0, 0), rgba(43, 43, 43))' : 'linear-gradient(to left, rgba(166, 0, 0, 0.5), rgba(43, 43, 43, 0.5))',
                             ':hover': {
                                 backgroundImage: `linear-gradient(to right, #a60000, #2b2b2b)`,
                                 color: 'white',
                                 border: '2px solid #000000'
                             }
-                        }}>{disableEditing ? "Enable Editing" : "Disable Editing"}</Button>
+                        }}>{enableEditing ? (<React.Fragment><ToggleOnIcon sx={{ color: "#60ff52" }} /> Edit</React.Fragment>) : (<React.Fragment><ToggleOffIcon sx={{ color: "#c70000" }} /> Edit</React.Fragment>)}
+                    </Button>
                 </Grid>
 
                 <Grid item xs={12} pt='5px'>
@@ -147,7 +173,7 @@ const Payments = () => {
                                 </TableRow>
                             ))}
                         </TableHead>
-                        <TableBody>
+                        <TableBody {...getTableBodyProps()}>
                             {rows.map((row, i) => {
                                 prepareRow(row)
                                 return (
@@ -159,11 +185,16 @@ const Payments = () => {
                                         }}>
                                         {row.cells.map(cell => {
                                             return (
-                                                <TableCell {...cell.getCellProps()}
+                                                <TableCell {...cell.getCellProps({
+                                                    style: {
+                                                        width: cell.column.width || 300,
+                                                    },
+                                                })}
                                                     sx={{
                                                         width: '200px',
                                                         padding: '5px',
                                                         // border: 'solid 1px gray',
+                                                        borderLeft: '1px dotted #000',
                                                         backgroundColor: isEven(i) ? '#ffffff' : '#ededed',
                                                         '&:hover': {
                                                             backgroundColor: '#fffbf2',
@@ -177,18 +208,22 @@ const Payments = () => {
                                         <TableCell
                                             sx={{
                                                 width: '200px',
-                                                padding: '5px',
+                                                pl: '20px',
+                                                pr: '0px',
+                                                pb: '5px',
+                                                pt: '5px',
                                                 // border: 'solid 1px gray',
                                                 background: isEven(i) ? '#ffffff' : '#ededed',
+                                                borderLeft: '1px dotted #000',
                                                 '&:hover': {
                                                     backgroundColor: '#fffbf2',
                                                 },
                                             }}
                                         >
 
-                                            <Button disabled={disableEditing} onClick={() => console.log(row.values)}
+                                            <Button disabled={!enableEditing} onClick={() => console.log(row.values)}
                                                 sx={{
-                                                    my: 2, color: '#dedede', border: '2px solid #000000', mt: '0px', mb: '0px', ml: '5px', mr: '5px', backgroundImage: disableEditing ? 'linear-gradient(to left, rgba(166, 0, 0, 0.5), rgba(43, 43, 43, 0.5))' : 'linear-gradient(to left, rgba(166, 0, 0), rgba(43, 43, 43))',
+                                                    my: 2, color: '#dedede', border: '2px solid #000000', mt: '0px', mb: '0px', ml: '5px', mr: '5px', backgroundImage: enableEditing ? 'linear-gradient(to left, rgba(166, 0, 0), rgba(43, 43, 43))' : 'linear-gradient(to left, rgba(166, 0, 0, 0.5), rgba(43, 43, 43, 0.5))',
                                                     ':hover': {
                                                         backgroundImage: `linear-gradient(to right, #a60000, #2b2b2b)`,
                                                         color: 'white',
@@ -196,9 +231,9 @@ const Payments = () => {
                                                     }
                                                 }}><EditIcon></EditIcon></Button>
 
-                                            <Button disabled={disableEditing} onClick={() => console.log(row.values)}
+                                            <Button disabled={!enableEditing} onClick={() => console.log(row.values)}
                                                 sx={{
-                                                    my: 2, color: '#dedede', border: '2px solid #000000', mt: '0px', mb: '0px', ml: '5px', backgroundImage: disableEditing ? 'linear-gradient(to left, rgba(166, 0, 0, 0.5), rgba(43, 43, 43, 0.5))' : 'linear-gradient(to left, rgba(166, 0, 0), rgba(43, 43, 43))',
+                                                    my: 2, color: '#dedede', border: '2px solid #000000', mt: '0px', mb: '0px', ml: '5px', backgroundImage: enableEditing ? 'linear-gradient(to left, rgba(166, 0, 0), rgba(43, 43, 43))' : 'linear-gradient(to left, rgba(166, 0, 0, 0.5), rgba(43, 43, 43, 0.5))',
                                                     ':hover': {
                                                         backgroundImage: `linear-gradient(to right, #a60000, #2b2b2b)`,
                                                         color: 'white',
@@ -212,8 +247,7 @@ const Payments = () => {
                         </TableBody>
                     </Table>
                 </Grid>
-
-            </Grid>
+            </Grid >
         </Container >
     )
 }
