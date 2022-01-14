9040221e94f653a2ff458eb8ceddc884bd1a780f
NCHLab <mohamudc1@hotmail.com>
2022-01-14 00:56:38 +0000

    increase axios timeout

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,6 +1,6 @@
 const AXIOS_CONFIG = {
     baseURL: 'http://localhost:4004/api/v1',
-    timeout: 1000,
+    timeout: 4000,
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true,
 }
