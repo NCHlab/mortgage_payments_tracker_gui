7b194a4fe4e645731e59d4e631b6253798f509c4
NCHLab <mohamudc1@hotmail.com>
2022-01-15 20:15:29 +0000

    cleanup code

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,3 +1,6 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import PageNotFound from './pages/PageNotFound'
@@ -8,12 +11,7 @@ import AllOverpayments from './pages/AllOverpayments'
 import HomeImprovements from './pages/HomeImprovements'
 import AllHomeImprovements from './pages/AllHomeImprovements'
 
-import React, { useState } from 'react'
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
-
 import { useAuth } from './context/AuthContext';
-
-// import Header from "./components/generic/Header";
 import { Header } from './components/headerComponent';
 import { Footer } from './components/footerComponent/';
 
@@ -21,19 +19,14 @@ const Router = () => {
 
     const { loggedIn } = useAuth();
 
-
     return (
         <BrowserRouter>
-            {/* <Header2 /> */}
             <Header />
 
             <Routes>
-
                 <Route path="/" element={<HomePage />} />
                 <Route path="/home" element={<Navigate to="/" />} />
                 <Route path="/404_not_found" element={<PageNotFound />} />
-
-
 
                 {!loggedIn ? (
                     <React.Fragment>
@@ -53,33 +46,6 @@ const Router = () => {
                     </React.Fragment>
 
                 )}
-
-
-
-                {/* {!loggedIn && (
-                    <React.Fragment>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/home" element={<Navigate to="/" />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="*" element={<Navigate to="/" />} />
-                    </React.Fragment>
-                )}
-
-
-
-                {loggedIn && (
-                    <React.Fragment>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/home" element={<Navigate to="/" />} />
-                        <Route path="/payments" element={<HomePage />} />
-                        <Route path="/payments/all" element={<HomePage />} />
-                        <Route path="/overpayments" element={<HomePage />} />
-                        <Route path="/overpayments/all" element={<HomePage />} />
-                        <Route path="/home_improvements" element={<HomePage />} />
-                        <Route path="/home_improvements/all" element={<HomePage />} />
-                    </React.Fragment>
-
-                )} */}
             </Routes>
             <Footer />
         </BrowserRouter>
