05c336a61e4385603976adab93896f8fb13ba0d0
NCHLab <mohamudc1@hotmail.com>
2022-01-17 22:55:15 +0000

    fix redundant code

--- src/components/controls/CSVDownloadButton.jsx
+++ src/components/controls/CSVDownloadButton.jsx
@@ -1,13 +1,13 @@
 import { Button } from '@mui/material';
 import DownloadOutlinedIcon from '@mui/icons-material/DownloadOutlined';
 
-const CSVDownloadButton = ({ handleDownload, enableEditing, page }) => {
+const CSVDownloadButton = ({ handleDownload, page }) => {
     return (
         <Button
             variant="outlined"
             onClick={() => handleDownload("CSV", page)}
             sx={{
-                my: 1, color: '#dedede', border: '2px solid #000000', backgroundImage: enableEditing ? 'linear-gradient(to left, rgba(166, 0, 0), rgba(43, 43, 43))' : 'linear-gradient(to left, rgba(166, 0, 0), rgba(43, 43, 43))',
+                my: 1, color: '#dedede', border: '2px solid #000000', backgroundImage: 'linear-gradient(to left, rgba(166, 0, 0), rgba(43, 43, 43))',
                 ':hover': {
                     backgroundImage: `linear-gradient(to right, #a60000, #2b2b2b)`,
                     color: '#36c9ff',
--- src/components/controls/XLSXDownloadButton.jsx
+++ src/components/controls/XLSXDownloadButton.jsx
@@ -1,13 +1,13 @@
 import { Button } from '@mui/material';
 import DownloadOutlinedIcon from '@mui/icons-material/DownloadOutlined';
 
-const XLSXDownloadButton = ({ handleDownload, enableEditing, page }) => {
+const XLSXDownloadButton = ({ handleDownload, page }) => {
     return (
         <Button
             variant="outlined"
             onClick={() => handleDownload("XLSX", page)}
             sx={{
-                my: 1, color: '#dedede', border: '2px solid #000000', backgroundImage: enableEditing ? 'linear-gradient(to left, rgba(166, 0, 0), rgba(43, 43, 43))' : 'linear-gradient(to left, rgba(166, 0, 0), rgba(43, 43, 43))',
+                my: 1, color: '#dedede', border: '2px solid #000000', backgroundImage: 'linear-gradient(to left, rgba(166, 0, 0), rgba(43, 43, 43))',
                 ':hover': {
                     backgroundImage: `linear-gradient(to right, #a60000, #2b2b2b)`,
                     color: '#36c9ff',
